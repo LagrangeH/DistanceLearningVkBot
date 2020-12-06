0fe4d60afed60897c93eb27cc6ecf727d93a9ff9
LagrangeH <maxsergeev39@gmail.com>
2020-12-06 21:31:21 +0300

    Added on/off function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import vk_api
 import time
+from loguru import logger as log
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
+
+import data
 
 
 def auth_handler():
@@ -11,25 +16,40 @@ def auth_handler():
     return key, remember_device
 
 
-def send_message(vk, id_type, id, message=None, attachment=None):
-    vk.method('messages.send', {id_type: id, 'message': message, 'random_id': get_random_id(), 'attachment': attachment})
+def send_message(vk, id_type, id, message, attachment=None):
+    vk.method('messages.send',
+              {id_type: id, 'message': message, 'random_id': get_random_id(), 'attachment': attachment})
 
 
-log, pas = "Ввести номер телефона сюда", "Ввести пароль сюда"
-vk = vk_api.VkApi(login=log, password=pas, auth_handler=auth_handler, app_id=2685278)
-vk.auth(token_only=True)
+vk = vk_api.VkApi(token=data.token)
+vk.get_api()
 longpoll = VkLongPoll(vk)
+bot_status = True
 student_id = 286259343  # id Светы
-print("Bot Launched")
+log.add("debug.log", format="{time} {level} {message}", level="DEBUG", rotation="100 KB", compression="zip")
+log.info("Bot Launched")
 
-while True:
+
+@log.catch()
+def run():
+    global bot_status
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             response = event.text.lower()
-            if event.user_id == student_id and response == "+":
-                time.sleep(4)
-                send_message(vk, 'chat_id', event.chat_id, message="+")
-                print("+")
-            if event.user_id == 273207132 and response == "test":
-                send_message(vk, 'user_id', event.user_id, message="Bot Launched")
-                print("test")
+            if event.user_id == student_id and response == "+" and bot_status:
+                time.sleep(3)
+                send_message(vk, 'chat_id', event.chat_id, "+")
+                log.info("'+' has been sent")
+            if event.user_id == 273207132:  # from me
+                if response == "test":
+                    send_message(vk, 'user_id', event.user_id, bot_status)
+                if response == "on":
+                    bot_status = True
+                    send_message(vk, 'user_id', event.user_id, 'Status: ON')
+                if response == "off":
+                    bot_status = False
+                    send_message(vk, 'user_id', event.user_id, 'Status: OFF')
+
+
+if __name__ == '__main__':
+    run()
